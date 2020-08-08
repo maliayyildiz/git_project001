741ef7f0f59613c1abb197768656bbfea2d1301b
maliayyildiz <maliayyildiz61@gmail.com>
2020-08-08 20:32:49 +0200

    last commit from local master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -3,7 +3,7 @@ package first_package;
 public class FirstClass{
 
   public static void main(String [] args) {
-
+System.out.println("i want to have conflict");
 System.out.println("Our first commit from local repo");
 System.out.println("Our second commit from local repository");
 System.out.println("now I am in ");
